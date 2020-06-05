19e64ed97c8297262200fc87b0a2e41fbd2e0ef5
billow <billow.fun@gmail.com>
2020-06-05 18:28:18 +0800

    using flex 2.5.35 + gcc 6.5.0

--- shell.nix
+++ shell.nix
@@ -1,23 +1,7 @@
 with import <nixpkgs> {};
 
-let
-  flex = (import (builtins.fetchGit {
-    # Descriptive name to make the store path easier to identify
-    name = "flex-2.6.0";
-    url = "https://github.com/nixos/nixpkgs-channels/";
-    ref = "refs/heads/nixos-20.03";
-    rev = "1971f39ab19dde7d74841bbb4c1e50c94e873830";
-  }) {}).flex;
-  gcc = (import (builtins.fetchGit {
-    # Descriptive name to make the store path easier to identify
-    name = "gcc-5.3.0";
-    url = "https://github.com/nixos/nixpkgs-channels/";
-    ref = "refs/heads/nixos-20.03";
-    rev = "d5eec25ff97b0d21fe8f08b2a3d70ddf50d0fc87";
-  }) {}).gcc;
-in
-  stdenv.mkDerivation rec {
+gcc6Stdenv.mkDerivation rec {
     name = "not-cool-env";
     src = ./.;
-    buildInputs = with pkgs; [ flex gcc cowsay ];
-  }
+    buildInputs = with pkgs; [ flex_2_5_35 ];
+}
